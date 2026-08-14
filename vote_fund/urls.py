d538badadbbfdcf3ad22537e2794c97f623915ff
OPTIMUS <83286997+deOpty@users.noreply.github.com>
2026-08-14 16:21:39 +0000

    Added Password Recovery, Show Password toggle, Scanner HTTPS error handling

--- vote_fund/urls.py
+++ vote_fund/urls.py
@@ -3,11 +3,18 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from voting import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('ussd/callback/', views.ussd_callback, name='ussd_callback'),
     path('admin/', admin.site.urls),
     path('', include('voting.urls')),
+  
+    # Password Reset Routes
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='voting/password_reset.html'), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='voting/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='voting/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='voting/password_reset_complete.html'), name='password_reset_complete'),
 ]
 
 if settings.DEBUG:
